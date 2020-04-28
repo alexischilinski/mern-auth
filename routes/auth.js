080ec71fb0278d0eaf0a3786130e62e7f1a573d3
alexiscait142 <chilinski.a@gmail.com>
2020-04-28 14:10:28 -0400

    check to see if email exists in db

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,10 @@ router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 
+    //check if user is already in database
+    const emailExist = await User.findOne({email: req.body.email})
+    if(emailExist) return res.status(400).send('email exists in db')
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
