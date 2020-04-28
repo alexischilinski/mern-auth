5fb7b604b5fd5c5d4b562e55f90edd9bc42f8d57
alexiscait142 <chilinski.a@gmail.com>
2020-04-28 14:07:41 -0400

    create separate validation file

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,33 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const { registerValidation, loginValidation } = require('../validation')
 
 //validation
-const Joi = require('@hapi/joi')
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-})
+// const Joi = require('@hapi/joi')
+// const schema = Joi.object({
+//     name: Joi.string().min(6).required(),
+//     email: Joi.string().min(6).required().email(),
+//     password: Joi.string().min(6).required()
+// })
 
 router.post('/register', async (req, res) => {
 
     //validate data before making user
-    const validation = schema.validate(req.body)
-    res.send(validation)
+    // const { error } = schema.validate(req.body)
+    const { error } = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
 
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // })
-    // try{
-    //     const savedUser = await user.save()
-    //     res.send(savedUser)
-    // }catch(err){
-    //     res.status(400).send(err)
-    // }
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try{
+        const savedUser = await user.save()
+        res.send(savedUser)
+    }catch(err){
+        res.status(400).send(err)
+    }
 })
 
 // router.post('/login', (req, res)=>{
