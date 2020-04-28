2fd104bbdbc5290eff5607db97b730686de6b8e8
alexiscait142 <chilinski.a@gmail.com>
2020-04-28 13:54:07 -0400

    validate user post

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,31 @@
 const router = require('express').Router()
 const User = require('../models/User')
 
+//validation
+const Joi = require('@hapi/joi')
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
 
 router.post('/register', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    try{
-        const savedUser = await user.save()
-        res.send(savedUser)
-    }catch(err){
-        res.status(400).send(err)
-    }
+
+    //validate data before making user
+    const validation = schema.validate(req.body)
+    res.send(validation)
+
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+    // try{
+    //     const savedUser = await user.save()
+    //     res.send(savedUser)
+    // }catch(err){
+    //     res.status(400).send(err)
+    // }
 })
 
 // router.post('/login', (req, res)=>{
